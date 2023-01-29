029524d3183f63c88ac0c6e77b91e46bcba8ce23
Lisnyi <russelhobbbs@gmail.com>
2023-01-29 19:18:46 +0200

    Update validationSchema.js

--- src/components/forms/validationSchema.js
+++ src/components/forms/validationSchema.js
@@ -2,11 +2,11 @@ import * as yup from 'yup';
 
 const schema = yup.object().shape({
     word: yup.string()
-        .matches(/^[a-zA-Zа-яА-ЯЁёІіЇїЄє]+$/, "Must contain only letters")
-        .required(),
+        .matches(/^[a-zA-Zа-яА-ЯЁёІіЇїЄє\'\-]+$/, "Поле має містити тільки літери та символи(`)(-)")
+        .required("Не забудьте додати слово"),
     translate: yup.string()
-        .matches(/^[a-zA-Zа-яА-ЯЁёІіЇїЄє]+$/, "Must contain only letters")
-        .required()
+        .matches(/^[a-zA-Zа-яА-ЯЁёІіЇїЄє\'\-]+$/, "Поле має містити тільки літери та символи(`)(-)")
+        .required("Не забудьте додати переклад")
 });
 
 export default schema
